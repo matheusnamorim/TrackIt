1af171831e04c63a4fe3400ff11326d71e14445f
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-08-03 00:30:07 -0300

    Arquivos iniciais

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import GlobalStyle from "../styles/globalStyles"
+import MainRoute from "./MainRoute/MainRoute";
+
+export default function App(){
+    return (
+        <>
+            <GlobalStyle/>
+            <MainRoute />
+        </>
+    );
+}
---
+++ src/components/MainRoute/MainRoute.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import logo from '../../assets/imgs/logo.png';
+
+
+export default function MainRoute(){
+    return (
+        <>
+        </>
+    );
+}
